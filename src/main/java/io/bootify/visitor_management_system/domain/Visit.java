175c37fc0b62ca9e6f8d2307896bd02483c24c02
Arpit319-byte <arpitchauhanupal@gmail.com>
2023-07-29 18:58:35 +0530

    updated visit fields

--- src/main/java/io/bootify/visitor_management_system/domain/Visit.java
+++ src/main/java/io/bootify/visitor_management_system/domain/Visit.java
@@ -1,13 +1,7 @@
 package io.bootify.visitor_management_system.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import io.bootify.visitor_management_system.model.VisitStatus;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,7 +18,8 @@ public class Visit {
     private Long id;
 
     @Column(nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private VisitStatus status;
 
     @Column(nullable = false)
     private LocalDateTime inTime;
